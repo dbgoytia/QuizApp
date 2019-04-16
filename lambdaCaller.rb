e60ceef5353dcd1ea1fe386769150ab9178f841e
EC2 Default User <ec2-user@ip-172-31-23-203.us-east-2.compute.internal>
2019-04-16 23:42:43 +0000

    added lambdas

--- lambdaCaller.rb
+++ lambdaCaller.rb
@@ -1,5 +1,26 @@
+require 'faraday'
+require 'json'
 require 'sinatra'
 
-get '/enter' do
-  'Hello world!'
+get '/getScores' do
+  URL_LAMBDAS = "https://74js1u1ok0.execute-api.us-west-2.amazonaws.com/default/getScores"
+  connection = Faraday.new
+  response = connection.get URL_LAMBDAS
+  return response.body
+end
+
+get '/getQuestions/:number' do
+  number = params[:number]
+  url_lambda = "https://60ozc8b5i3.execute-api.us-west-2.amazonaws.com/default/getQuestions?numberOfQuestions=#{number}"
+  connection = Faraday.new
+  response = connection.get url_lambda
+  return response.body
+end
+
+get '/getQuestions/:number' do
+  number = params[:number]
+  url_lambda = "https://60ozc8b5i3.execute-api.us-west-2.amazonaws.com/default/getQuestions?numberOfQuestions=#{number}"
+  connection = Faraday.new
+  response = connection.get url_lambda
+  return response.body
 end
