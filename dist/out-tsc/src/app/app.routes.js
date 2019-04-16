eb80dc6223de07b754a6d17782101697ade68220
A01376119 <a01376119@itesm.mx>
2019-04-16 16:00:02 -0500

    ca

--- dist/out-tsc/src/app/app.routes.js
+++
@@ -1,9 +0,0 @@
-import { RouterModule } from '@angular/router';
-// Main applications:
-import { InputformComponent } from './components/inputform/inputform.component';
-var APP_ROUTES = [
-    { path: 'prinicpal', component: InputformComponent },
-    { path: '**', pathMatch: 'full', redirectTo: 'principal' }
-];
-export var APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
-//# sourceMappingURL=app.routes.js.map
