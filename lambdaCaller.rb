4c11e726eb9d5a729b49c00eb7cd054edeb52f90
EC2 Default User <ec2-user@ip-172-31-23-203.us-east-2.compute.internal>
2019-04-22 23:06:58 +0000

    TEST: enabling CORS authenticatoin protocols

--- lambdaCaller.rb
+++ lambdaCaller.rb
@@ -1,6 +1,8 @@
 require 'faraday'
 require 'json'
 require 'sinatra'
+require 'sinatra/cross_origin'
+
 
 get '/getScores' do
   URL_LAMBDAS = "https://74js1u1ok0.execute-api.us-west-2.amazonaws.com/default/getScores"
@@ -10,6 +12,7 @@ get '/getScores' do
 end
 
 get '/getQuestions/:number' do
+  response.headers['Access-Control-Allow-Origin'] = '*'
   number = params[:number]
   url_lambda = "https://60ozc8b5i3.execute-api.us-west-2.amazonaws.com/default/getQuestions?numberOfQuestions=#{number}"
   connection = Faraday.new
