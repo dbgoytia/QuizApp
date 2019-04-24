502ba2d936592e32acc700236cca2c014f314002
EC2 Default User <ec2-user@ip-172-31-43-159.us-west-2.compute.internal>
2019-04-24 02:04:21 +0000

    nueva lambda que obtine scores

--- lambdaCaller.rb
+++ lambdaCaller.rb
@@ -13,17 +13,19 @@ set :protection, false
 
 
 get '/getScores' do
-  URL_LAMBDAS = "https://74js1u1ok0.execute-api.us-west-2.amazonaws.com/default/getScores"
+  URL_LAMBDAS = "https://mbx4dy66a6.execute-api.us-west-2.amazonaws.com/default/scores"
   connection = Faraday.new
   response = connection.get URL_LAMBDAS
   return response.body
 end
 
-get '/getQuestions/:number' do
-  number = params[:number]
-  url_lambda = "https://60ozc8b5i3.execute-api.us-west-2.amazonaws.com/default/getQuestions?numberOfQuestions=#{number}"
+get '/insertScore' do
+  url_lambda = "https://mbx4dy66a6.execute-api.us-west-2.amazonaws.com/default/scores"
+  payload = {"initials": "MARK", "questions": 5, "score": 99}
+  puts "HOLIZ"
+  puts payload
   connection = Faraday.new
-  response = connection.get url_lambda
+  response = connection.post url_lambda, payload
   return response.body
 end
 
